c6bdf470dfb6539cee297c3f62f7ec42eb00dfae
arshad <arshadpatel2001@gmail.com>
2021-03-18 22:54:41 +0530

    added splash screen ui and animations

--- app/src/main/java/com/example/myapplication/splash/SplashActivity.kt
+++ app/src/main/java/com/example/myapplication/splash/SplashActivity.kt
@@ -4,15 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.example.myapplication.MainActivity
 import com.example.myapplication.R
 import com.example.myapplication.onboarding.OnboardingActivity
 
 class SplashActivity : AppCompatActivity() {
     lateinit var handler: Handler
+    lateinit var bottomAnim: Animation
+    lateinit var topAnim: Animation
+    lateinit var cardView: CardView
+    lateinit var textView1: TextView
+    lateinit var textView2: TextView
+    lateinit var textView3: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        cardView = findViewById(R.id.icon)
+        textView1 = findViewById(R.id.appname)
+        textView2 = findViewById(R.id.middle)
+        textView3 = findViewById(R.id.bottom)
+
+        cardView.setAnimation(topAnim)
+        textView1.setAnimation(bottomAnim)
+        textView2.setAnimation(bottomAnim)
+        textView3.setAnimation(bottomAnim)
 
         handler = Handler()
         handler.postDelayed(
@@ -23,6 +47,6 @@ class SplashActivity : AppCompatActivity() {
                     startActivity(intent)
                     finish()
 
-                } , 2000)
+                }, 2000)
     }
 }
